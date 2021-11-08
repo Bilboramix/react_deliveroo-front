cd0adabe320e71d4a39e0f08a1bd9a44e8659f8d
bilbo <axel.catois@gmail.com>
2021-11-08 13:42:28 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
